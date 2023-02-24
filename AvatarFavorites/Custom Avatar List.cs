857f6761c938cfb220f14c7f79e6dc3f070622f0
プレイヤー-2 <95395808+gadiiell@users.noreply.github.com>
2023-02-24 13:43:31 -0600

    Update Custom Avatar List.cs

--- AvatarFavorites/Custom Avatar List.cs
+++ AvatarFavorites/Custom Avatar List.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Vanilla.Misc.AvatarFavorites
+namespace Example.Misc.AvatarFavorites
 {
     internal class Class1
     {
